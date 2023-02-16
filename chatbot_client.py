415e86b8d1168d6c388f07e22a0d1b39046f7384
SvadilfariYT <dominik.sekular@gmail.com>
2023-02-16 18:13:50 +0100

    Entity Recognition überarbeitet und Logger ergänzt

--- chatbot_client.py
+++ chatbot_client.py
@@ -24,14 +24,22 @@ if __name__ == '__main__':
         chatbot = Chatbot(respond, "SnapeBot")
 
     else:
+        log = open('log.txt', 'a')
         print(Fore.GREEN + get_answer_of_type(phrases.greetings) + Style.RESET_ALL)
         user_input = input(">>> ").strip()
+
+        log.write('\nUser: ' + user_input)    
         while "exit" not in user_input.lower():
             
-            answer = get_response(user_input)       
+            answer = get_response(user_input)
+            log.write('\nWizardBot: ' + answer)    
 
             print(answer + Style.RESET_ALL)
             print("________________________________________________")
             user_input = input(">>> ").strip()
+            log.write('\nUser: ' + user_input)    
 
-        print(get_answer_of_type(phrases.farewells))
+        exit_answer = get_answer_of_type(phrases.farewells)
+        print(exit_answer)
+        log.write('\nWizardBot: ' + exit_answer)
+        log.write('\n_______________________________________')    
