9ef1f3e6cf1d9a2a233e73505a7e94e219ed6db3
Ana287 <ana_2807@hotmail.de>
2023-02-23 11:29:54 +0100

    fixed special patterns again

--- special_patterns.py
+++ special_patterns.py
@@ -3,7 +3,6 @@ introductions = [
      r'.*My name is (.*)',
      ["Greetings, {0}.",
       "Nice to meet you, {0}.",
-      "It's a pleasure to meet you, {0}.",
       "How old are you, {0}?",
       "Well met, {0}!",
       "Ah, {0}. I've heard your name before. You're the one who is well known around the wizarding world!",
@@ -16,23 +15,7 @@ introductions = [
       "Charmed, I'm sure, {0}. You seem like a wizard with potential. Have you given any thought to which magical subjects you'd like to study?",
       "{0}, where have I heard that name before? Ah yes, {0} was a once a disputable Death Eater...",
       "Pleased to make your acquaintance, {0}. You strike me as someone who would appreciate the finer points of Transfiguration.",
-      "Welcome, {0}, to the wonderful world of wizardry! I can tell just by looking at you that you're destined for greatness.",
-      "{0}, I presume? I've been looking forward to meeting you. You have a reputation for [insert something related to the wizarding world here].",
-      "Well met, {0}!",
-      "Ah, {0}. I've heard your name before. You're the one who [insert something related to the wizarding world here]!",
-      "Greetings, {0}. May I ask what brings you to Hogwarts?",
-      "I'm pleased to make your acquaintance, {0}. I'm sure we'll be seeing more of each other around the castle.",
-      "Hello, {0}. You know, you remind me of a certain [character from the Harry Potter books]. Have you ever read them?",
-      "A pleasure to meet you, {0}. Are you by any chance a fan of Quidditch?",
-      "Welcome to Hogwarts, {0}. I hope you enjoy your stay. Do you have any questions about the school or the wizarding world?",
-      "{0}! That's a wonderful name. I knew a {0} once - they were quite talented at [insert something related to magic here].",
-      "Nice to meet you, {0}. I'm [insert name here], a professor here at Hogwarts. What brings you to my office?",
-      "{0}, you say? That's a name with a lot of history in the wizarding world. Do you know any of your ancestors' accomplishments?",
-      "Charmed, I'm sure, {0}. You seem like a wizard with potential. Have you given any thought to which magical subjects you'd like to study?",
-      "{0}, {0}, where have I heard that name before? Ah yes, [insert something related to the wizarding world here].",
-      "Pleased to make your acquaintance, {0}. You strike me as someone who would appreciate the finer points of Transfiguration.",
-      "Welcome, {0}, to the wonderful world of wizardry! I can tell just by looking at you that you're destined for greatness.",
-      "{0}, I presume? I've been looking forward to meeting you. You have a reputation for [insert something related to the wizarding world here]."]
+      "Welcome, {0}, to the wonderful world of wizardry! I can tell just by looking at you that you're destined for greatness."]
       ]
 ]
 
